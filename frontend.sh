530689cff02edd270cf5f1023b0b2b9558f612c9
shuja-git <shuja.devops@gmail.com>
2023-08-03 04:59:52 -0700

    frontend setup

--- frontend.sh
+++ frontend.sh
@@ -4,4 +4,4 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 systemctl enable nginx
-systemctl restart nginx
+systemctl start nginx
